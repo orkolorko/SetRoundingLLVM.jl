d5ab31d9d3959c282faf1202bf42f7a169af50c6
Isaia Nisoli <nisoli@im.ufrj.br>
2022-11-10 14:32:18 +0900

    Added Multithread and LinearAlgebra tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using SetRoundingLLVM
 using Test
 
-@testset "SetRoundingLLVM.jl" begin
+@testset "Testing Rounding" begin
     rnd = llvm_rounding()
     
     llvm_setrounding(RoundToZero)
@@ -67,4 +67,64 @@ using Test
     @test c == 0.39999999999999997
     @test d == 0.4
 
+end
+
+@testset "Testing multithreading" begin
+    N = Threads.nthreads()
+
+    function testing_roundings_up(rounding::RoundingMode)
+        x = 1.0
+        x = llvm_setrounding(rounding) do
+            x+eps(1.0)/2
+        end
+        return x
+    end
+
+    function testing_roundings_down(rounding::RoundingMode)
+        x = 1.0
+        x = llvm_setrounding(rounding) do
+            x-eps(1.0)/4
+        end
+        return x
+    end
+
+
+    a = SetRoundingLLVM.from_llvm.([Int32(i%4) for i in 1:N])
+
+    v = zeros(N)
+    Threads.@threads for i = 1:N
+        v[Threads.threadid()] = testing_roundings_up(a[Threads.threadid()])
+    end
+
+    @test all(v[[i%4==1 for i in 1:N]] .== 1.0) #Nearest 
+    @test all(v[[i%4==2 for i in 1:N]] .== nextfloat(1.0)) #Up 
+    @test all(v[[i%4==3 for i in 1:N]] .== 1.0) #Down
+    @test all(v[[i%4==0 for i in 1:N]] .== 1.0) #ToZero
+
+    v = zeros(N)
+    Threads.@threads for i = 1:N
+        v[Threads.threadid()] = testing_roundings_down(a[Threads.threadid()])
+    end
+    
+    @test all(v[[i%4==1 for i in 1:N]] .== 1.0) #Nearest 
+    @test all(v[[i%4==2 for i in 1:N]] .== 1.0) #Up 
+    @test all(v[[i%4==3 for i in 1:N]] .== prevfloat(1.0)) #Down
+    @test all(v[[i%4==0 for i in 1:N]] .== prevfloat(1.0)) #ToZero
+end
+
+@testset "Testing using external Linear Algebra library" begin
+    using LinearAlgebra
+
+    v = [1.0; 1.0]
+    w = [1.0; eps(1.0)/2]
+    
+    x = llvm_setrounding(RoundUp) do
+        BLAS.dot(v, w)
+    end
+    @test x == nextfloat(1.0)
+
+    x = llvm_setrounding(RoundNearest) do
+        BLAS.dot(v, w)
+    end
+    @test x == 1.0
 end
